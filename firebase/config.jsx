4e686be593012b30060ef539e51370728fba2e4c
Juan Hernández <hajjose7@gmail.com>
2021-07-07 12:49:43 -0400

    Creando los archivos de configuración de Firebase

---
+++ firebase/config.jsx
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABSMDmXoOHZFTpbFvu9ByS96fTD7339a0",
+    authDomain: "product-hund-1f258.firebaseapp.com",
+    projectId: "product-hund-1f258",
+    storageBucket: "product-hund-1f258.appspot.com",
+    messagingSenderId: "767044859201",
+    appId: "1:767044859201:web:ec9cb0ac275d6bb204ddc0"
+};
+
+export default firebaseConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import App from 'next/app';
+import firebase, { FirebaseContext } from '../firebase';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = props => {
+
+  const { Component, pageProps } = props;
+
+  return (
+    <FirebaseContext.Provider
+      value={{
+        firebase
+      }}
+    >
+      <Component {...pageProps} />
+    </FirebaseContext.Provider>
+  )
 }
 
-export default MyApp
+export default MyApp;
