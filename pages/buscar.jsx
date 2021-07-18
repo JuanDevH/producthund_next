d1683ae5819ac7b2ad66ab82c7e8f2d98ca66028
Juan Hernández <hajjose7@gmail.com>
2021-07-18 10:18:04 -0400

    Proyecto Finalizado con Nextjs

--- pages/buscar.jsx
+++ pages/buscar.jsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from 'react';
 import Layout from '../components/layout/Layout';
+import { useRouter } from 'next/router';
+import DetallesProductos from '../components/layout/DetallesProductos';
+import useProductos from '../hooks/useProductos';
 
-const Buscar = () => (
-  <div>
-    <Layout>
-      <h1>Buscar</h1>
-    </Layout>
-  </div>
-)
+const Buscar = () => {
+
+  const router = useRouter();
+  const { query: { q } } = router;
+
+  // Todos los productos
+  const { productos } = useProductos('creado');
+  const [ resultado, setResultado ] = useState([]);
+
+  useEffect(() => {
+    const busqueda = q.toLowerCase();
+    const filtro = productos.filter(producto => {
+      return (
+        producto.nombre.toLowerCase().includes(busqueda) ||
+        producto.descripcion.toLowerCase().includes(busqueda)
+      )
+    });
+    setResultado(filtro);
+  }, [q, productos]);
+
+  return (
+    <div>
+      <Layout>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {resultado.map(producto => (
+                <DetallesProductos 
+                  key={producto.id}
+                  producto={producto}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  )
+}
 
 export default Buscar;
